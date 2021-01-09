5ccaec162086670757f4019415cd4a5dc763c0e7
tjaskolka <n4zud@aol.com>
2021-01-08 19:36:56 -0500

    added year js

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,5 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('lastupdated').textContent = new Date().toLocaleDateString('en-US', options);
+
+const year = {year: 'numeric'};
+document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', year);
