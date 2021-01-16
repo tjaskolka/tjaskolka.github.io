ca51d13fc67bbb959bb81816aac0a305849f3810
tjaskolka <n4zud@aol.com>
2021-01-16 10:02:32 -0500

    testing for submission

---
+++ lesson2/js/currentdate.js
@@ -0,0 +1,5 @@
+//provide information on document last update
+document.getElementById('lastupdated').textContent = document.lastModified;
+
+//provide current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
