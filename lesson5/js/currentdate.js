dfef0f87c4ce96a499c5a28a264f2d72f7bb3d5a
tjaskolka <n4zud@aol.com>
2021-02-05 14:06:50 -0500

    added content & js day of week rotation

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -10,3 +10,19 @@ document.getElementById('calendarDate').innerHTML = day + ", " + date + " " + mo
 
 //provide current year
 document.getElementById('currentyear').innerHTML = year;
+
+if (day === "Friday") {
+    document.getElementById("announcement").style.display = "block";
+}
+
+var week = days.slice(0);
+var count = new Date().getDay();
+for (i = 0 ; i < count; i++) {
+    week.push(week.shift());
+}
+
+document.getElementById('day1').innerHTML = week[0];
+document.getElementById('day2').innerHTML = week[1];
+document.getElementById('day3').innerHTML = week[2];
+document.getElementById('day4').innerHTML = week[3];
+document.getElementById('day5').innerHTML = week[4];
