8cdee93f45664c3d1933749b81b352764f9273a7
tjaskolka <n4zud@aol.com>
2021-02-13 14:59:25 -0500

    windchill final

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,8 @@
-let speed = parseFloat(document.getElementById("speed").value);
-let temp = parseFloat(document.getElementById("temp").value);
-let chill = windChill (temp, speed);
-document.getElementById("windchill").innerHTML = Math.round(chill * 10)/10;
+let speed = parseFloat(document.querySelector("#speed").textContent);
+let temp = parseFloat(document.querySelector("#temp").textContent);
+document.getElementById("windchill").innerHTML = windChill (temp, speed);
 
 function windChill(temp, speed) {
-    let end = 35.74 + .6215*temp = 35.75*speed**.16 + .4275*temp*speed**.16;
+    let end = Math.round(35.74 + .6215*temp - 35.75*speed**.16 + .4275*temp*speed**.16);
     return end;
 }
