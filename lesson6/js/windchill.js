051bac6065b5101f25baa43d00ade17a853ef1b8
tjaskolka <n4zud@aol.com>
2021-02-13 16:14:13 -0500

    submit assignment

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,14 @@
 let speed = parseFloat(document.querySelector("#speed").textContent);
 let temp = parseFloat(document.querySelector("#temp").textContent);
-document.getElementById("windchill").innerHTML = windChill (temp, speed);
+document.getElementById("windchill").innerHTML = windChill(temp, speed);
 
 function windChill(temp, speed) {
-    let end = Math.round(35.74 + .6215*temp - 35.75*speed**.16 + .4275*temp*speed**.16);
+    if (temp <= 50 && speed > 3){
+    let end = Math.round(35.74 + .6215*temp - 35.75*speed**.16 + .4275*temp*speed**.16) + "&#8457;";
     return end;
+    }
+    else
+    {
+        return "N/A";
+    }
 }
