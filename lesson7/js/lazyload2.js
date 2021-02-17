74a9a630f5a7ef8b412a094eaff415993e8e72d7
tjaskolka <n4zud@aol.com>
2021-02-16 19:18:57 -0500

    correction

--- lesson7/js/lazyload2.js
+++ lesson7/js/lazyload2.js
@@ -6,10 +6,10 @@ const loadImages = (image) => {
         image.removeAttribute('data-src');
     };
 };
-
+/*
 imagesToLoad.forEach((img) => {
     loadImages(img);
-});
+});*/
 
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
