71aface71b8fd56f694abbff2aaee4e9fd695bd2
tjaskolka <n4zud@aol.com>
2021-02-21 00:34:39 -0500

    7

--- lesson7/js/currentdate.js
+++ lesson7/js/currentdate.js
@@ -1,6 +1,6 @@
 //compose date consisting of day, date month year
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var day = days[new Date().getDay()];
 var date = new Date().getDate();
 var month = months[new Date().getMonth()];
@@ -14,16 +14,3 @@ document.getElementById('currentyear').innerHTML = year;
 if (day === "Friday") {
     document.getElementById("announcement").style.display = "block";
 }
-
-var week = days.slice(0);
-var count = new Date().getDay();
-for (i = 0 ; i < count; i++) {
-    week.push(week.shift());
-}
-
-document.getElementById('day1').innerHTML = week[0];
-document.getElementById('day2').innerHTML = week[1];
-document.getElementById('day3').innerHTML = week[2];
-document.getElementById('day4').innerHTML = week[3];
-document.getElementById('day5').innerHTML = week[4];
-
