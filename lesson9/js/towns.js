6ad754a88a13ec87e48e90092aee0eb00f109482
tjaskolka <n4zud@aol.com>
2021-03-07 21:57:16 -0500

    lesson 9 final

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -1,5 +1,44 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
+function createCard(towns, townname) {
+    const town3 = towns.filter(d => d.name == townname);
+
+  town3.forEach(town =>  {
+      let card = document.createElement('section');
+      
+      let h2 = document.createElement('h2');
+      let motto = document.createElement('h3');
+      let founded = document.createElement('p');
+      let population = document.createElement('p');
+      let rainfall = document.createElement('p');
+      let img = document.createElement('img');
+      let div = document.createElement('div');
+    
+      h2.innerHTML = `${town.name}`;
+      motto.innerHTML = `${town.motto}`;
+      founded.innerHTML = `${town.yearFounded}`;
+      population.innerHTML = `${town.currentPopulation}`;
+      rainfall.innerHTML = `Annual Rain Fall: ${town.averageRainfall}"`;
+
+      img.setAttribute('src', `images/${town.photo}`);
+      img.setAttribute('alt', `${town.name} town photo`);      
+
+      founded.innerHTML = `Year Founded: ${town.yearFounded}`;
+
+      population.innerHTML = `Population: ${town.currentPopulation}`;
+
+      div.append(h2);
+      div.append(motto);
+      div.append(founded);
+      div.append(population);
+      div.append(rainfall);
+      card.append(img);
+      card.append(div);
+
+      document.querySelector('div.towns').appendChild(card);
+});
+};
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -8,32 +47,9 @@ fetch(requestURL)
   .then(function (jsonObject) {
 
   const towns = jsonObject['towns'];
-
-  const town3 = towns.filter(d => d.name == 'Fish Haven' || d.name == 'Preston');
-
-  town3.forEach(town =>  {
-      let card = document.createElement('section');
-      
-      let h2 = document.createElement('h2');
-  /*    let img = document.createElement('img');
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-    */
-      h2.innerHTML = `${town.name}`;
-/*
-      img.setAttribute('src', `/images/${town.name}`);
-      img.setAttribute('alt', `town photo`)      
-
-      p1.innerHTML = `Year Founded: ${town.yearFounded}`;
-
-      p2.innerHTML = `Population: ${town.durrentPopulation}`;
-*/
-      card.append(h2);
-  /*    card.append(p1);
-      card.append(p2);
-      card.append(img);
- */
-      document.querySelector('div.towns').appendChild(card);
+    createCard(towns, "Preston");
+    createCard(towns, "Soda Springs");
+    createCard(towns, "Fish Haven");
+  
   });  
 
-});
