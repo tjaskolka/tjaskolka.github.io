e8f9405ae310ec311008280d66499f5021bd1242
tjaskolka <n4zud@aol.com>
2021-03-13 12:52:35 -0500

    upload progress for help

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -2,11 +2,19 @@
 
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=aee2c79ef21500c6b3d3a695fdea645a";
 
-fetch(apiURL)
+function convert(kelvin) {
+        let fahrenheit = Math.round((1.8*(kelvin - 273) + 32)*10/10);
+        return fahrenheit;
+      }
+
+      fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
 
-        const temp = Math.round((1.8*(jsObject.main.temp - 273) + 32)*10/10);
+     
+
+        //const temp = Math.round((1.8*(jsObject.main.temp - 273) + 32)*10/10);
+        const temp = convert(jsObject.main.temp);
         document.getElementById('temp').textContent = temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
         document.getElementById('current').textContent = jsObject.weather[0].main;
@@ -19,41 +27,37 @@ fetch(apiURL)
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=aee2c79ef21500c6b3d3a695fdea645a";
 
 function createCard(fiveDay) {  
-    const forecast = fiveDay.filter(d => d.name == );
+    const forecast = fiveDay.filter(d => d.dt_txt.includes("18:00:00")  );
+console.log(forecast);
+  forecast.forEach(day =>  {
+      let i = 1;
+    let card = document.createElement('div');
 
-  town3.forEach(town =>  {
-      let card = document.createElement('section');
-      
-      let h2 = document.createElement('h2');
-      let motto = document.createElement('h4');
-      let founded = document.createElement('p');
-      let population = document.createElement('p');
-      let rainfall = document.createElement('p');
-      let img = document.createElement('img');
-      let div = document.createElement('div');
+    //return weather icon
+    let img = document.createElement('img');
+    let imgsrc = "https://openweathermap.org/img/w/" + `${day.weather[0].icon}` + ".png";
+    img.setAttribute('src', imgsrc);
+    card.append(img);
+
+    //return temperature
+    let temp = document.createElement('p');
+    temp.innerHTML = convert(`${day.main.temp}`)+'$#8457;';
+    card.append(temp);
+
+    //return weather condition
+    let h4 = document.createElement('h4');
+    h4.innerHTML = `${day.weather[0].main}`;
+    card.append(h4);
+
+
     
-      h2.innerHTML = `${town.name}`;
-      motto.innerHTML = `${town.motto}`;
-      founded.innerHTML = `${town.yearFounded}`;
-      population.innerHTML = `${town.currentPopulation}`;
-      rainfall.innerHTML = `Annual Rain Fall: ${town.averageRainfall}"`;
+    
+    document.querySelector('section.forecast').append(card);
 
-      img.setAttribute('src', `images/${town.photo}`);
-      img.setAttribute('alt', `${town.name} town photo`);      
 
-      founded.innerHTML = `Year Founded: ${town.yearFounded}`;
 
-      population.innerHTML = `Population: ${town.currentPopulation}`;
+      i++;
 
-      div.append(h2);
-      div.append(motto);
-      div.append(founded);
-      div.append(population);
-      div.append(rainfall);
-      card.append(img);
-      card.append(div);
-
-      document.querySelector('div.towns').appendChild(card);
 });
 };
 
@@ -66,7 +70,5 @@ fetch(forecastURL)
 
   const fiveDay = jsonObject['list'];
     createCard(fiveDay);
- //   createCard(towns, "Soda Springs");
- //   createCard(towns, "Fish Haven");
   
   });  
