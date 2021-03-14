16bc4693aa820f1cc8c3047563aeac4cb246e405
tjaskolka <n4zud@aol.com>
2021-03-13 20:44:40 -0500

    lesson 10 upload

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -28,33 +28,29 @@ const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473
 
 function createCard(fiveDay) {  
     const forecast = fiveDay.filter(d => d.dt_txt.includes("18:00:00")  );
-console.log(forecast);
+    console.log(forecast);
+let i = 1;
   forecast.forEach(day =>  {
-      let i = 1;
-    let card = document.createElement('div');
+
+    let div = document.getElementById('day'+i);
 
     //return weather icon
     let img = document.createElement('img');
-    let imgsrc = "https://openweathermap.org/img/w/" + `${day.weather[0].icon}` + ".png";
-    img.setAttribute('src', imgsrc);
-    card.append(img);
+    img.src = "https://openweathermap.org/img/w/" + `${day.weather[0].icon}` + ".png";
+    img.alt = `${day.weather[0].description}`;
 
     //return temperature
     let temp = document.createElement('p');
-    temp.innerHTML = convert(`${day.main.temp}`)+'$#8457;';
-    card.append(temp);
+    temp.innerHTML = convert(`${day.main.temp}`)+'\u00B0F';
 
     //return weather condition
     let h4 = document.createElement('h4');
     h4.innerHTML = `${day.weather[0].main}`;
-    card.append(h4);
-
-
     
-    
-    document.querySelector('section.forecast').append(card);
-
-
+ //add elements to each div
+div.appendChild(temp);
+div.appendChild(img);
+div.appendChild(h4);
 
       i++;
 
--- lesson10/js/weekdays.js
+++ lesson10/js/weekdays.js
@@ -7,8 +7,23 @@ for (i = 0 ; i < count; i++) {
     week.push(week.shift());
 }
 
-document.getElementById('day1').innerHTML = week[0];
-document.getElementById('day2').innerHTML = week[1];
-document.getElementById('day3').innerHTML = week[2];
-document.getElementById('day4').innerHTML = week[3];
-document.getElementById('day5').innerHTML = week[4];
+let div1 = document.getElementById('day1');
+let p1 = document.createElement('p');
+p1.innerHTML = week[1];
+div1.appendChild(p1);
+let div2 = document.getElementById('day2');
+let p2 = document.createElement('p');
+p2.innerHTML = week[2];
+div2.appendChild(p2);
+let div3 = document.getElementById('day3');
+let p3 = document.createElement('p');
+p3.innerHTML = week[3];
+div3.appendChild(p3);
+let div4 = document.getElementById('day4');
+let p4 = document.createElement('p');
+p4.innerHTML = week[4];
+div4.appendChild(p4);
+let div5 = document.getElementById('day5');
+let p5 = document.createElement('p');
+p5.innerHTML = week[5];
+div5.appendChild(p5);
