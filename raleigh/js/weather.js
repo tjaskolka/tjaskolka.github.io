86b73236d5cb615e5bdcbd09b5387439af8f5de9
tjaskolka <n4zud@aol.com>
2021-03-30 22:19:31 -0400

    beginning files

---
+++ raleigh/js/weather.js
@@ -0,0 +1,3 @@
+const weatherCall = "https://api.openweathermap.org/data/2.5/onecall?lat=35.850148&lon=-78.658447&exclude=minutely,hourly&appid=aee2c79ef21500c6b3d3a695fdea645a&units=imperial"
+
+console.log(weatherCall);
